05981712d25157a7cecd2ca529f061e10632eb34
Schury1998 <99131504+Schury1998@users.noreply.github.com>
2023-11-02 13:01:51 +0100

    changes in the arudino uno folder for testing, will be implemented in the CANanalyse.ino file

--- code_for_arduino_uno/simulate_CAN2COM 2/src/main.cpp
+++ code_for_arduino_uno/simulate_CAN2COM 2/src/main.cpp
@@ -5,7 +5,32 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("CAN MSG: 0x200 [7] <1F:C0:00:10:00:03:01>");
+  if (Serial.available() > 0) //Send Messages
+  {
+    String incomingString = Serial.readString();
+    int last_index_of_I = incomingString.lastIndexOf('I'); //Wenn Zeichen nicht enthalten return von -1
+    if(last_index_of_I >= 0) 
+    {
+      int str_len = incomingString.length() + 1; 
+      char char_array[str_len];
+      incomingString.toCharArray(char_array, str_len);
+      char *teil;
+      int can_message[3];
+      teil = strtok(char_array, "I"); 
+      int i = 0;
+      while (teil != NULL) 
+      {
+        can_message[i] = atoi(teil);
+        teil = strtok(NULL, "I");
+        ++i;
+      }
+      //Serial.println(can_message[0]); //ID
+      //Serial.println(can_message[1]); //DLC
+      //Serial.println(can_message[2]); //Payload
+    }
+  }
+  
+  Serial.print("CAN MSG: 0x200 [7] <1F:C0:00:10:00:03:01>"); //Recive Messages
   Serial.println();
   delay(80);
   Serial.print("CAN MSG: 0x710 [8] <02:10:03:00:00:00:00:00>");
